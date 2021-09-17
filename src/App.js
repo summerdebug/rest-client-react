29780aaf122eb5cbcb6c8dd12b6b60ca15399b9c
summerdebug <summerdebug@gmail.com>
2021-09-17 10:59:14 +0300

    URL field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3" controlId="urlGroup">
+                  <Form.Label>URL</Form.Label>
+                  <Form.Control type="text" placeholder="http://localhost:8080/api/validate/trades" />
+                </Form.Group></Col>
+            </Row>
+            <Row>
+              <Col>Row 2, Col 1</Col>
+            </Row>
+            <Row>
+              <Col>Row 3, Col 1</Col>
+            </Row>
+          </Form>
+      </Container>
     </div>
   );
 }
